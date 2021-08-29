edd452b756a7c2197e827f1e41eb7d934040d999
tsgoswami <trishnangshugoswami@yahoo.in>
2021-08-30 01:21:27 +0530

    google oauth authentication created for sending emails

--- backend/src/utility/mailer.ts
+++ backend/src/utility/mailer.ts
@@ -1,12 +1,30 @@
 import nodemailer, { SentMessageInfo } from 'nodemailer';
 import { MailRequestModel } from '../interfaces';
+import { google } from 'googleapis';
+
+const OAuth2 = google.auth.OAuth2;
+
+const client = new OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    process.env.REDIRECT_URL
+)
+
+client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN
+});
+
+const accessToken = client.getAccessToken();
 
 const transporter = nodemailer.createTransport({
-    service: process.env.MAIL_SERVER_HOST,
+    host: process.env.MAIL_SERVER_HOST,
+    port: 465,
+    secure: true,
     auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
-    },
+        type: 'OAuth2',
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET
+    }
 });
 
 export class Mailer {
@@ -23,6 +41,12 @@ export class Mailer {
                 bcc: mailData.reciever.bcc,
                 subject: mailData.subject,
                 text: mailData.content,
+                auth:{
+                    user: process.env.MAIL_USER,
+                    refreshToken: process.env.REFRESH_TOKEN,
+                    accessToken: accessToken,
+                    expires: 1484314697598
+                }
             };
             const response: SentMessageInfo = await transporter.sendMail(email);
             return Promise.resolve(response);
