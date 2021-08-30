1254326c94c9aa7a3fcff9535dfc08c1cc883550
tsgoswami <trishnangshugoswami@yahoo.in>
2021-08-30 23:54:39 +0530

    Disclaimer updated

--- frontend/src/main/components/DisclaimerDialog/DisclaimerDialog.jsx
+++ frontend/src/main/components/DisclaimerDialog/DisclaimerDialog.jsx
@@ -24,7 +24,7 @@ const DisclaimerDialog = ({
             <DialogTitle id="alert-dialog-title">Disclaimer</DialogTitle>
             <DialogContent>
                 <DialogContentText id="alert-dialog-description">
-                    Please note that a lot of fraud is going on around us, in connection to these supplies. Thus we advise caution on the part of the patient party before involving in monetary transaction with any supplier. Hence, please personally verify the product. If you suspect any foul play, please contact the concerned authorities. It is hereby declared that Artificial Brix is not going to be liable for any kind of fraud related to the services you seek.
+                    Please note that a lot of fraud is going on around us, in connection to these supplies. Thus we advise caution on the part of the patient party before involving in monetary transaction with any supplier. Hence, please personally verify the product. If you suspect any foul play, please contact the concerned authorities. It is hereby declared that the owner of this app is not going to be liable for any kind of fraud related to the services you seek.
             </DialogContentText>
             </DialogContent>
             <Grid container justify='center' alignItems='center'>
